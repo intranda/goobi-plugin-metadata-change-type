41dd40e7ebbf130ab9b52467b9c5dd96dbf3896e
Robert Sehr <robert.sehr@intranda.com>
2022-11-15 06:21:22 +0100

    infrastructure

---
+++ plugin/src/main/java/de/intranda/goobi/plugins/ChangeTypeMetadataPlugin.java
@@ -0,0 +1,55 @@
+package de.intranda.goobi.plugins;
+
+/**
+ * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
+ *
+ * Visit the websites for more information.
+ *          - https://goobi.io
+ *          - https://www.intranda.com
+ *          - https://github.com/intranda/goobi
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+import java.util.HashMap;
+
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginGuiType;
+import org.goobi.production.enums.PluginReturnValue;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.enums.StepReturnValue;
+import org.goobi.production.plugin.interfaces.IMetadataEditorExtension;
+import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
+
+import de.sub.goobi.config.ConfigPlugins;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@PluginImplementation
+@Log4j2
+public class ChangeTypeMetadataPlugin implements IMetadataEditorExtension {
+
+    private static final long serialVersionUID = 3644294549629729916L;
+
+    @Getter
+    private String pagePath = "/uii/plugin_metadata_changeType.xhtml";
+
+    @Getter
+    private String title = "intranda_metadata_changeType";
+    
+    @Getter
+    private String modalId = "changeTypeModal";
+    
+    
+    
+}
